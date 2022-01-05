ad9ddcafd879669690a4e8495e12a1b8c28b9abf
hotaek <kht072623@naver.com>
2022-01-05 17:20:58 +0900

    [CHORE] packaging

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,7 @@
 object Apps {
-    const val compileSdk = 30
+    const val compileSdk = 31
     const val minSdk = 24
-    const val targetSdk = 30
+    const val targetSdk = 31
     const val versionCode = 1
     const val versionName = "1.0.0"
     const val pacakageName = "com.nadosunbae_android"
