6cf485746452e1422914e3ae66703581b064e387
gimangi <khk211113@naver.com>
2022-01-12 19:23:00 +0900

    #13 [UI] Add icon_diamond

--- app/src/main/java/com/nadosunbae_andorid/presentation/ui/review/ReviewDetailActivity.kt
+++ app/src/main/java/com/nadosunbae_andorid/presentation/ui/review/ReviewDetailActivity.kt
@@ -9,4 +9,11 @@ class ReviewDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_detail)
     }
+
+
+    /*
+    배경 샘플 이미지
+    https://user-images.githubusercontent.com/37872134/149095580-e2987cf0-39ff-4663-ab91-4899620d8203.png
+     */
+
 }
