d9e0395b39380dcf19225917046f546d25e9bb28
hotaek <kht072623@naver.com>
2022-01-13 01:00:32 +0900

    #25 [FEAT] question_write_all_edit_text

---
+++ app/src/main/java/com/nadosunbae_andorid/presentation/ui/classroom/QuestionWriteActivity.kt
@@ -0,0 +1,21 @@
+package com.nadosunbae_andorid.presentation.ui.classroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nadosunbae_andorid.R
+import com.nadosunbae_andorid.databinding.ActivityQuestionWriteBinding
+import com.nadosunbae_andorid.presentation.base.BaseActivity
+
+class QuestionWriteActivity : BaseActivity<ActivityQuestionWriteBinding>(R.layout.activity_question_write) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_write)
+    }
+
+
+
+
+
+    //제목 부분 밑줄 변경
+
+}
