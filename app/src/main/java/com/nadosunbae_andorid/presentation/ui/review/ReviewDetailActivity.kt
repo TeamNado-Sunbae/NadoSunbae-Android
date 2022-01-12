531a0d3d5abed698ff28d0e6cbd23a991cf93b09
gimangi <khk211113@naver.com>
2022-01-12 17:45:29 +0900

    #13 [FEAT] Create ReviewDetailActivity

---
+++ app/src/main/java/com/nadosunbae_andorid/presentation/ui/review/ReviewDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.nadosunbae_andorid.presentation.ui.review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nadosunbae_andorid.R
+
+class ReviewDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_detail)
+    }
+}
