89fe78ee458ea0affd0e8c7af66256ae959a0078
lhb8106 <lhb8106@naver.com>
2022-01-14 15:59:02 +0900

    #29 [MODIFY] modify  SignIn Layout

--- app/src/main/java/com/nadosunbae_andorid/presentation/ui/sign/SignInActivity.kt
+++ app/src/main/java/com/nadosunbae_andorid/presentation/ui/sign/SignInActivity.kt
@@ -4,20 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import com.nadosunbae_andorid.R
 import com.nadosunbae_andorid.databinding.ActivitySignInBinding
 import com.nadosunbae_andorid.presentation.base.BaseActivity
+import java.util.regex.Pattern
 
 class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        onView()
+        onViewId()
         moveFindPw()
         moveSignUp()
+        onViewPw()
     }
 
-    private fun onView() {
+    //id editText textwatcher
+    private fun onViewId() {
         binding.etSignInId.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
@@ -33,28 +37,77 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sig
                     binding.imgSignInIdCancel.isSelected = true
                 }
 
+                isEmailPattern()
+                isEmptyText()
+            }
+        })
+
+        binding.imgSignInIdCancel.setOnClickListener {
+            binding.etSignInId.setText(null)
+            binding.clLogin.isSelected = false
+        }
+    }
+
+    //이메일 양식 체크
+    private fun isEmailPattern() {
+        val pattern: Pattern = Patterns.EMAIL_ADDRESS
+
+        if (pattern.matcher(binding.etSignInId.text).matches()) {
+            binding.clLogin.isSelected = true
+        } else {
+            binding.clLogin.isSelected = false
+        }
+    }
+
+
+    //pw editText textWatcher
+    private fun onViewPw() {
+        binding.etSignInPw.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
                 if (binding.etSignInPw.text.toString() == "") {
                     binding.imgSignInPwCancel.isSelected = false
                 } else {
                     binding.imgSignInPwCancel.isSelected = true
                 }
 
-                if (binding.etSignInId.text.toString() != "" && binding.etSignInPw.text.toString()!="") {
-                    binding.clLogin.isSelected = true
-                } else {
-                    binding.clLogin.isSelected = false
-                }
+                isEmailPattern()
+                isEmptyText()
+
             }
         })
 
+        binding.imgSignInPwCancel.setOnClickListener {
+            binding.etSignInPw.setText(null)
+            binding.clLogin.isSelected = false
+
+        }
     }
 
+    //id editText & pw editText 빈칸 여부
+    private fun isEmptyText() {
+        if (binding.etSignInId.text.toString() != "" && binding.etSignInPw.text.toString()!="") {
+            binding.clLogin.isSelected = true
+        } else {
+            binding.clLogin.isSelected = false
+        }
+    }
+
+    //회원가입 페이지로 이동
     private fun moveSignUp() {
         binding.textSignInSignup.setOnClickListener {
             startActivity(Intent(this, SignUpAgreementActivity::class.java))
         }
     }
 
+    //비밀번호 찾기 페이지로 이동
     private fun moveFindPw() {
         binding.textSignInFindpw.setOnClickListener {
             startActivity(Intent(this, FindPwActivity::class.java))
