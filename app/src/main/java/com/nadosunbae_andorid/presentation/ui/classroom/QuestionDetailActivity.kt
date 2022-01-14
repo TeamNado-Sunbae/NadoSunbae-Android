a007f3a2019ea0936a2e6f9b78d25273f639a9a4
hotaek <kht072623@naver.com>
2022-01-14 14:21:12 +0900

    #36 [FEAT] question_detail_xml

---
+++ app/src/main/java/com/nadosunbae_andorid/presentation/ui/classroom/QuestionDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.nadosunbae_andorid.presentation.ui.classroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nadosunbae_andorid.R
+import com.nadosunbae_andorid.databinding.ActivityQuestionDetailBinding
+import com.nadosunbae_andorid.presentation.base.BaseActivity
+
+class QuestionDetailActivity : BaseActivity<ActivityQuestionDetailBinding>(R.layout.activity_question_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/nadosunbae_andorid/presentation/ui/classroom/SeniorPersonalFragment.kt
+++ app/src/main/java/com/nadosunbae_andorid/presentation/ui/classroom/SeniorPersonalFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nadosunbae_andorid.R
-
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -14,7 +13,7 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SeniorPersonalFragment.newInstance] factory method to
+ * Use the [SeniorPersnoalFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class SeniorPersonalFragment : Fragment() {
@@ -30,10 +29,8 @@ class SeniorPersonalFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_senior_personal, container, false)
     }
@@ -45,16 +42,15 @@ class SeniorPersonalFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SeniorPersonalFragment.
+         * @return A new instance of fragment SeniorPersnoalFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SeniorPersonalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                SeniorPersnoalFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
