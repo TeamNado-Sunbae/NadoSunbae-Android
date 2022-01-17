fda4b0cdbff51c0faf351d9c5fc95202fdc7cbbe
hotaek <kht072623@naver.com>
2022-01-17 20:51:11 +0900

    #53 [FEAT] classroom_review_view

---
+++ app/src/main/java/com/nadosunbae_android/presentation/ui/classroom/ClassRoomReviewFragment.kt
@@ -0,0 +1,21 @@
+package com.nadosunbae_android.presentation.ui.classroom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nadosunbae_android.R
+import com.nadosunbae_android.databinding.FragmentClassRoomReviewBinding
+import com.nadosunbae_android.presentation.base.BaseFragment
+
+
+class ClassRoomReviewFragment : BaseFragment<FragmentClassRoomReviewBinding>(R.layout.fragment_class_room_review) {
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+}
