b281dbb102f832c554ae25bb18a5c067b1e677b5
hotaek <kht072623@naver.com>
2022-01-17 21:04:12 +0900

    #53 [FEAT] classroom_review

--- app/src/main/java/com/nadosunbae_android/presentation/ui/classroom/SeniorPersonalFragment.kt
+++ app/src/main/java/com/nadosunbae_android/presentation/ui/classroom/SeniorPersonalFragment.kt
@@ -2,19 +2,30 @@ package com.nadosunbae_android.presentation.ui.classroom
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.nadosunbae_android.R
 import com.nadosunbae_android.data.model.response.classroom.ResponseClassRoomMainData
 import com.nadosunbae_android.databinding.FragmentSeniorBinding
 import com.nadosunbae_android.databinding.FragmentSeniorPersonalBinding
 import com.nadosunbae_android.presentation.base.BaseFragment
 import com.nadosunbae_android.presentation.ui.classroom.adapter.ClassRoomQuestionMainAdapter
+import com.nadosunbae_android.presentation.ui.main.viewmodel.MainViewModel
 
 class SeniorPersonalFragment : BaseFragment<FragmentSeniorPersonalBinding>(R.layout.fragment_senior_personal) {
     private lateinit var classRoomQuestionMainAdapter : ClassRoomQuestionMainAdapter
-
+    private val mainViewModel: MainViewModel by activityViewModels{
+        object : ViewModelProvider.Factory{
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return MainViewModel() as T
+            }
+        }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initSeniorQuestion()
+        goClassRoomReview()
     }
 
     //선배에게 온 1:1 질문 목록
@@ -89,4 +100,11 @@ class SeniorPersonalFragment : BaseFragment<FragmentSeniorPersonalBinding>(R.lay
 
     }
 
+
+    private fun goClassRoomReview(){
+        binding.imgSeniorPersonalClassReviewArrow.setOnClickListener {
+            mainViewModel.classRoomFragmentNum.value = 5
+        }
+
+    }
 }
