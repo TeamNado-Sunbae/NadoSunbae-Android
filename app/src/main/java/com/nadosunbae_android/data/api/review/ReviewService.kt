c80d05d7570421d1fba81e7e27b9542a2efb1cca
gimangi <khk211113@naver.com>
2022-01-19 05:01:38 +0900

    #49 [FEAT] ReviewService

--- app/src/main/java/com/nadosunbae_android/data/api/review/ReviewService.kt
+++ app/src/main/java/com/nadosunbae_android/data/api/review/ReviewService.kt
@@ -3,16 +3,12 @@ package com.nadosunbae_android.data.api.review
 import com.nadosunbae_android.data.model.request.review.RequestReviewListData
 import com.nadosunbae_android.data.model.response.review.ResponseReviewListData
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ReviewService {
 
-    @GET("/review-post/list")
+    @POST("review-post/list")
     fun getReviewList(
-        @Header("accesstoken") token: String,
         @Query("sort") sort: String = "recent",
         @Body requestBody: RequestReviewListData
     ) : Call<ResponseReviewListData>
