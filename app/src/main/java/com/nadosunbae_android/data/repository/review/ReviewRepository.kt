f8f0b16d25985b7a668e57e9e42727d534d25c17
gimangi <khk211113@naver.com>
2022-01-19 05:04:21 +0900

    #49 [FEAT] ReviewRepository

--- app/src/main/java/com/nadosunbae_android/data/repository/review/ReviewRepository.kt
+++ app/src/main/java/com/nadosunbae_android/data/repository/review/ReviewRepository.kt
@@ -5,8 +5,8 @@ import com.nadosunbae_android.data.model.response.review.ResponseReviewListData
 import retrofit2.Response
 
 interface ReviewRepository {
-    fun getReviewList(
-        token: String, sort: String = "recent", body: RequestReviewListData,
+
+    fun getReviewList(sort: String = "recent", body: RequestReviewListData,
         onResponse: (Response<ResponseReviewListData>) -> Unit,
         onFailure: (Throwable) -> Unit
         )
