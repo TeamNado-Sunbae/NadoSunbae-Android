39d7dc0175bb9bdfabaa99366142f52a191366df
gimangi <khk211113@naver.com>
2022-01-19 18:12:23 +0900

    #88 [FEAT] MainDataSource

--- app/src/main/java/com/nadosunbae_android/data/datasource/remote/main/MainDataSource.kt
+++ app/src/main/java/com/nadosunbae_android/data/datasource/remote/main/MainDataSource.kt
@@ -1,4 +1,14 @@
 package com.nadosunbae_android.data.datasource.remote.main
 
+import com.nadosunbae_android.data.model.response.sign.ResponseMajorData
+import retrofit2.Response
+
 interface MainDataSource {
+
+    fun getMajorList(
+        universityId: Int,
+        filter: String = "all",
+        onResponse: (Response<ResponseMajorData>) -> Unit,
+        onFailure: (Throwable) -> Unit
+    )
 }
--- app/src/main/java/com/nadosunbae_android/data/datasource/remote/main/MainDataSourceImpl.kt
+++ app/src/main/java/com/nadosunbae_android/data/datasource/remote/main/MainDataSourceImpl.kt
@@ -1,4 +1,21 @@
 package com.nadosunbae_android.data.datasource.remote.main
 
-class MainDataSourceImpl {
+import com.nadosunbae_android.data.api.ApiService
+import com.nadosunbae_android.data.model.response.sign.ResponseMajorData
+import com.nadosunbae_android.util.enqueueUtil
+import retrofit2.Response
+
+class MainDataSourceImpl() : MainDataSource {
+
+    override fun getMajorList(
+        universityId: Int,
+        filter: String,
+        onResponse: (Response<ResponseMajorData>) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ) {
+        ApiService.mainService.getMajorList(universityId, filter).enqueueUtil(
+            onResponse, onFailure
+        )
+    }
+
 }
