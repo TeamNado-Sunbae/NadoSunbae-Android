3ab55e61196fbcf680e09bc8d68372285cf366a7
gimangi <khk211113@naver.com>
2022-01-19 18:13:33 +0900

    #88 [FEAT] MainRepository

--- app/src/main/java/com/nadosunbae_android/data/repository/main/MainRepository.kt
+++ app/src/main/java/com/nadosunbae_android/data/repository/main/MainRepository.kt
@@ -1,4 +1,12 @@
 package com.nadosunbae_android.data.repository.main
 
+import com.nadosunbae_android.data.model.response.sign.ResponseMajorData
+import retrofit2.Response
+
 interface MainRepository {
+
+    fun getMajorList(universityId: Int, filter: String = "all",
+        onResponse: (Response<ResponseMajorData>) -> Unit,
+        onFailure: (Throwable) -> Unit
+        )
 }
