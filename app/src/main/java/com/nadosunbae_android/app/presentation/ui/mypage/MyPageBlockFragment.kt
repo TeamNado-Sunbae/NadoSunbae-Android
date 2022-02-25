99886da254858730d12a218340cfdc2e1c2ef426
lhb8106 <lhb8106@naver.com>
2022-02-25 15:14:00 +0900

    #255 [FEAT] mypage lookup block list end

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/mypage/MyPageBlockFragment.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/mypage/MyPageBlockFragment.kt
@@ -1,23 +1,14 @@
 package com.nadosunbae_android.app.presentation.ui.mypage
 
-import android.content.Intent
-import android.content.pm.PackageInfo
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import com.nadosunbae_android.app.BuildConfig
 import com.nadosunbae_android.app.R
-import com.nadosunbae_android.app.databinding.*
-import com.nadosunbae_android.app.presentation.base.BaseActivity
+import com.nadosunbae_android.app.databinding.FragmentMyPageBlockBinding
 import com.nadosunbae_android.app.presentation.base.BaseFragment
-import com.nadosunbae_android.app.presentation.ui.main.WebViewActivity
 import com.nadosunbae_android.app.presentation.ui.main.viewmodel.MainViewModel
-import com.nadosunbae_android.app.presentation.ui.mypage.adapter.MyPagePostAdapter
+import com.nadosunbae_android.app.presentation.ui.mypage.adapter.MyPageBlockAdapter
 import com.nadosunbae_android.app.presentation.ui.mypage.viewmodel.MyPageViewModel
-import com.nadosunbae_android.app.presentation.ui.review.ReviewDetailActivity.Companion.TAG
-import com.nadosunbae_android.domain.model.mypage.MyPageVersionData
+import com.nadosunbae_android.domain.model.mypage.MyPageBlockData
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -25,16 +16,32 @@ class MyPageBlockFragment : BaseFragment<FragmentMyPageBlockBinding>(R.layout.fr
 
     private val mainViewModel: MainViewModel by sharedViewModel()
     private val myPageViewModel: MyPageViewModel by viewModel()
+    private lateinit var myPageBlockAdapter: MyPageBlockAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         backBtn()
+        initBlockList()
     }
 
+    //뒤로가기 버튼 리스너
     private fun backBtn() {
         binding.imgMypageBlock.setOnClickListener {
             mainViewModel.mypageFragmentNum.value = 1
         }
+    }
+
+    //마이페이지 차단 리스트
+    private fun initBlockList() {
+        mainViewModel.signData.observe(viewLifecycleOwner) {
+            myPageViewModel.getMyPageBlock()
+        }
+        myPageBlockAdapter = MyPageBlockAdapter()
+        binding.rcMyPageQuestion.adapter = myPageBlockAdapter
+        myPageViewModel.blockList.observe(viewLifecycleOwner) {
+            myPageBlockAdapter.setBlockMain((it.data) as MutableList<MyPageBlockData.Data>)
+        }
+
     }
 
 }
