b40b752c9762e2fe7a7cb85d6d4888d25ae5d6cc
lhb8106 <lhb8106@naver.com>
2022-03-06 16:51:42 +0900

    #307 [FEAT] onboarding end

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingActivity.kt
@@ -1,5 +1,6 @@
 package com.nadosunbae_android.app.presentation.ui.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,12 +11,17 @@ import com.nadosunbae_android.app.databinding.ActivityMainBinding
 import com.nadosunbae_android.app.databinding.ActivityOnboardingBinding
 import com.nadosunbae_android.app.presentation.base.BaseActivity
 import com.nadosunbae_android.app.presentation.ui.main.viewmodel.MainViewModel
+import com.nadosunbae_android.app.presentation.ui.mypage.MyPageReplyActivity
+import com.nadosunbae_android.app.presentation.ui.sign.SignInActivity
+import com.nadosunbae_android.app.presentation.ui.sign.SignUpAgreementActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initPageMove()
 
 
         val pagerAdater = OnBoardingPagerAdapter(this)
@@ -34,6 +40,21 @@ class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.acti
                 2 -> OnBoardingThirdFragment()
                 else -> OnBoardingForthFragment()
             }
+        }
+    }
+
+
+    private fun initPageMove() {
+        binding.textSignup.setOnClickListener {
+            val intentSignUp = Intent(this, SignUpAgreementActivity::class.java)
+            startActivity(intentSignUp)
+            finish()
+        }
+
+        binding.textLogIn.setOnClickListener {
+            val intentLogIn = Intent(this, SignInActivity::class.java)
+            startActivity(intentLogIn)
+            finish()
         }
     }
 
