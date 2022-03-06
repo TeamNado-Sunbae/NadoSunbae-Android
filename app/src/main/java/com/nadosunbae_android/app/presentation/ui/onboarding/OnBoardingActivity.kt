4386f8eaf3cd1deebaa1e0bf5596e16dc479a45c
lhb8106 <lhb8106@naver.com>
2022-03-06 16:40:57 +0900

    #307 [FEAT] onboarding view end

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingActivity.kt
@@ -2,11 +2,39 @@ package com.nadosunbae_android.app.presentation.ui.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.nadosunbae_android.app.R
+import com.nadosunbae_android.app.databinding.ActivityMainBinding
+import com.nadosunbae_android.app.databinding.ActivityOnboardingBinding
+import com.nadosunbae_android.app.presentation.base.BaseActivity
+import com.nadosunbae_android.app.presentation.ui.main.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class OnBoardingActivity : AppCompatActivity() {
+class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+
+
+        val pagerAdater = OnBoardingPagerAdapter(this)
+        binding.vpOnboarding.adapter = pagerAdater
+        binding.dotsIndicator.setViewPager2(binding.vpOnboarding)
+
     }
+
+    private inner class OnBoardingPagerAdapter(fa : FragmentActivity) : FragmentStateAdapter(fa){
+        override fun getItemCount(): Int = 4
+
+        override fun createFragment(position: Int): Fragment {
+            return when(position){
+                0 -> OnBoardingFirstFragment()
+                1 -> OnBoardingSecondFragment()
+                2 -> OnBoardingThirdFragment()
+                else -> OnBoardingForthFragment()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingSecondFragment.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingSecondFragment.kt
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import com.nadosunbae_android.app.R
 
 
-class OnBoardingForthFragment : Fragment() {
+class OnBoardingSecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding_forth, container, false)
+        return inflater.inflate(R.layout.fragment_on_boarding_second, container, false)
     }
 
 
