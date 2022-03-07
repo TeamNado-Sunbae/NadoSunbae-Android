1ce4129951bf8dca2b58be4d3c970b55c3ccea84
gimangi <khk211113@naver.com>
2022-03-07 15:48:13 +0900

    #314 [MODIFY] add method in report dialog

--- app/src/main/java/com/nadosunbae_android/app/util/ToastUtil.kt
+++ app/src/main/java/com/nadosunbae_android/app/util/ToastUtil.kt
@@ -1,4 +1,11 @@
 package com.nadosunbae_android.app.util
 
+import android.content.Context
+import android.widget.Toast
+
 object ToastUtil {
+}
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
