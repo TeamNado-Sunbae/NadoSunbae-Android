90a44bd14f12ceb11946429e94558c65005d253a
hotaek <kht072623@naver.com>
2022-03-09 03:17:33 +0900

    #369 [UI] empty_view
    등록되지 않은 글

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/classroom/SeniorPersonalFragment.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/classroom/SeniorPersonalFragment.kt
@@ -69,6 +69,13 @@ class SeniorPersonalFragment :
         binding.rcSeniorPersonal.adapter = classRoomQuestionMainAdapter
         seniorPersonalViewModel.seniorQuestion.observe(viewLifecycleOwner) {
             Log.d("seniorQuestionAdapter", "좀 되라")
+            if(it.isEmpty()){
+                binding.textSeniorEmpty.visibility = View.VISIBLE
+                binding.rcSeniorPersonal.visibility = View.GONE
+            }else{
+                binding.textSeniorEmpty.visibility = View.GONE
+                binding.rcSeniorPersonal.visibility = View.VISIBLE
+            }
             classRoomQuestionMainAdapter.setQuestionMain(it as MutableList<ClassRoomData>)
         }
     }
