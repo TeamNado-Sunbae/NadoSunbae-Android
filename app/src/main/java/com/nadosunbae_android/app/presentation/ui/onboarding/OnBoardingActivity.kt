20ba519512091ba4135aee68fbec55ce256317d4
lhb8106 <lhb8106@naver.com>
2022-03-08 13:53:20 +0900

    #349 [MODIFY] change alret message

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/onboarding/OnBoardingActivity.kt
@@ -1,28 +1,28 @@
 package com.nadosunbae_android.app.presentation.ui.onboarding
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.nadosunbae_android.app.R
-import com.nadosunbae_android.app.databinding.ActivityMainBinding
 import com.nadosunbae_android.app.databinding.ActivityOnboardingBinding
 import com.nadosunbae_android.app.presentation.base.BaseActivity
-import com.nadosunbae_android.app.presentation.ui.main.viewmodel.MainViewModel
-import com.nadosunbae_android.app.presentation.ui.mypage.MyPageReplyActivity
+import com.nadosunbae_android.app.presentation.ui.classroom.adapter.ClassRoomAskEveryoneAdapter
 import com.nadosunbae_android.app.presentation.ui.sign.SignInActivity
 import com.nadosunbae_android.app.presentation.ui.sign.SignUpAgreementActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlinx.android.synthetic.main.activity_onboarding.*
+import java.lang.Boolean.getBoolean
 
 class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initPageMove()
-
 
         val pagerAdater = OnBoardingPagerAdapter(this)
         binding.vpOnboarding.adapter = pagerAdater
@@ -34,6 +34,7 @@ class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.acti
         override fun getItemCount(): Int = 4
 
         override fun createFragment(position: Int): Fragment {
+
             return when(position){
                 0 -> OnBoardingFirstFragment()
                 1 -> OnBoardingSecondFragment()
@@ -42,7 +43,6 @@ class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.acti
             }
         }
     }
-
 
     private fun initPageMove() {
         binding.textSignup.setOnClickListener {
@@ -57,5 +57,9 @@ class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.acti
             finish()
         }
     }
+
+
+
+
 
 }
