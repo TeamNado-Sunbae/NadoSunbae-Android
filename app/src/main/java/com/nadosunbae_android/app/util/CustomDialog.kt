e868d6a91335305465523f9b5d44bc802db1e493
hotaek <kht072623@naver.com>
2022-03-09 16:18:14 +0900

    #386 [MODIFY] dialog_update
    다이얼로그 내용 변경

--- app/src/main/java/com/nadosunbae_android/app/util/CustomDialog.kt
+++ app/src/main/java/com/nadosunbae_android/app/util/CustomDialog.kt
@@ -48,11 +48,14 @@ class CustomDialog(val context : Context) {
     }
 
     //작성 취소
-    fun writeCancelDialog(@LayoutRes layout : Int){
+    fun writeCancelDialog(@LayoutRes layout : Int, divisionNum : Int){
         dialog.setContentView(layout)
         dialog.window?.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
         dialog.window?.setBackgroundDrawableResource(R.drawable.rectangle_fill_white_8dp)
         dialog.show()
+        if(divisionNum == 1){
+            dialog.text_question_write_dialog.text = "페이지를 나가면 수정한 내용이 저장되지 않아요."
+        }
         dialog.text_question_write_dialog_out.setOnClickListener {
             onClickedListener.onClicked(1)
             dialog.dismiss()
