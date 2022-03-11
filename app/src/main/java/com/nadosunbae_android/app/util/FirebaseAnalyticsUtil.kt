65072bef95e2ed7d7caabdfc66df8698af18a632
gimangi <khk211113@naver.com>
2022-03-11 19:19:45 +0900

    #448 [MODIFY] select tab refactoring

--- app/src/main/java/com/nadosunbae_android/app/util/FirebaseAnalyticsUtil.kt
+++ app/src/main/java/com/nadosunbae_android/app/util/FirebaseAnalyticsUtil.kt
@@ -1,9 +1,9 @@
 package com.nadosunbae_android.app.util
 
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.ktx.logEvent
-import com.nadosunbae_android.app.di.NadoSunBaeApplication
-import java.util.*
+import com.google.firebase.ktx.Firebase
 
 object FirebaseAnalyticsUtil {
 
@@ -14,15 +14,19 @@ object FirebaseAnalyticsUtil {
     private const val QUESTION = "user_question"
     private const val QUESTION_TYPE ="question_type"
 
-    const val DAU = 1
-    const val WAU = 2
-    const val MAU = 3
+    object Tab {
+        const val REVIEW = "Review Tab"
+        const val CLASSROOM_QUESTION = "ClassRoom_Question Tab"
+        const val CLASSROOM_INFO = "ClassRoom_Info Tab"
+        const val NOTIFICATION = "Notification Tab"
+        const val MYPAGE = "MyPage Tab"
+    }
 
     object Post {
         const val REVIEW_NEW = "review_new"
         const val REVIEW_ADD = "review_additional"
         const val INFORMATION = "classroom_information"
-        const val QUESTION_ALL = "classromm_question_all"
+        const val QUESTION_ALL = "classroom_question_all"
         const val QUESTION_PERSONAL = "classroom_question_personal"
     }
 
@@ -32,27 +36,25 @@ object FirebaseAnalyticsUtil {
     }
 
 
-    private lateinit var _firebaseAnalytics: FirebaseAnalytics
-    val firebaseAnalytics: FirebaseAnalytics
-        get() = _firebaseAnalytics
+    val firebaseAnalytics: FirebaseAnalytics = Firebase.analytics
 
-    fun initFirebaseAnalytics(firebaseAnalytics: FirebaseAnalytics) {
-        this._firebaseAnalytics = firebaseAnalytics
-    }
+    fun get() = firebaseAnalytics
 
-    fun get() = _firebaseAnalytics
-
-    fun signup() = _firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SIGN_UP) {
+    fun signup() = firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SIGN_UP) {
         param(FirebaseAnalytics.Param.METHOD, "email")
     }
 
-    fun login() = _firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
+    fun login() = firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
             param(FirebaseAnalytics.Param.METHOD, "manual")
         }
 
 
-    fun autoLogin() = _firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
+    fun autoLogin() = firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
         param(FirebaseAnalytics.Param.METHOD, "auto")
+    }
+
+    fun selectTab(tab: String) = firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+        param(FirebaseAnalytics.Param.SCREEN_NAME, tab)
     }
 
 
@@ -74,11 +76,11 @@ object FirebaseAnalyticsUtil {
         }
     }
 
-    fun userPost(value: String = "other") = _firebaseAnalytics.logEvent(USER_PERSONAL) {
+    fun userPost(value: String = "other") = firebaseAnalytics.logEvent(USER_PERSONAL) {
         param(POST_TYPE, value)
     }
 
-    fun question(value: String = "other") = _firebaseAnalytics.logEvent(QUESTION) {
+    fun question(value: String = "other") = firebaseAnalytics.logEvent(QUESTION) {
         param(QUESTION_TYPE, value)
     }
 
