3909fd37c7af77618f468d7a5218e8be9bb27778
hotaek <kht072623@naver.com>
2022-03-23 00:39:12 +0900

    #473 [MODIFY] ContextCompat
    변경

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/classroom/QuestionDetailActivity.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/classroom/QuestionDetailActivity.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.nadosunbae_android.app.R
 import com.nadosunbae_android.app.databinding.ActivityQuestionDetailBinding
@@ -173,9 +174,9 @@ class QuestionDetailActivity :
 
             override fun afterTextChanged(s: Editable?) {
                 if(s.isNullOrEmpty()){
-                    binding.imgQuestionCommentComplete.setBackgroundColor(getColor(R.color.gray_2))
+                    binding.imgQuestionCommentComplete.setBackgroundColor(ContextCompat.getColor(this@QuestionDetailActivity, R.color.gray_2))
                 }else{
-                    binding.imgQuestionCommentComplete.setBackgroundColor(getColor(R.color.main_default))
+                    binding.imgQuestionCommentComplete.setBackgroundColor(ContextCompat.getColor(this@QuestionDetailActivity, R.color.main_default))
                 }
             }
         })
