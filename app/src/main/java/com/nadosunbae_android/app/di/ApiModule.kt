9aaff28570999289287a8f590d9bc23ae2523de1
hotaek <kht072623@naver.com>
2022-05-30 23:19:52 +0900

    base_url_update

--- app/src/main/java/com/nadosunbae_android/app/di/ApiModule.kt
+++ app/src/main/java/com/nadosunbae_android/app/di/ApiModule.kt
@@ -13,7 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 private const val BASE_URL_PROD = "https://asia-northeast3-nadosunbae-server.cloudfunctions.net/api/"
 private const val BASE_URL_DEV = "https://asia-northeast3-nadosunbae-server-dev-90ac3.cloudfunctions.net/api/"
 
-private const val BASE_URL = BASE_URL_DEV
+private const val BASE_URL = BASE_URL_PROD
 
 val apiModule = module {
 
