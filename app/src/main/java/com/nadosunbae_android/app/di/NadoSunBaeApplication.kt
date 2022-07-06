af16f3acbfad770fe7ee86393d0d482e547fa7de
hotaek <kht072623@naver.com>
2022-07-07 01:10:03 +0900

    modify : ApiModule 및 NetworkModule 변경

--- app/src/main/java/com/nadosunbae_android/app/di/NadoSunBaeApplication.kt
+++ app/src/main/java/com/nadosunbae_android/app/di/NadoSunBaeApplication.kt
@@ -3,11 +3,13 @@ package com.nadosunbae_android.app.di
 import android.app.Application
 import android.content.Context
 import com.nadosunbae_android.app.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
 import org.koin.core.logger.Level
 
+@HiltAndroidApp
 class NadoSunBaeApplication : Application() {
     init{
         instance = this
@@ -17,20 +19,6 @@ class NadoSunBaeApplication : Application() {
         var instance: NadoSunBaeApplication? = null
         fun context() : Context {
             return instance!!.applicationContext
-        }
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        startKoin{
-            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
-            androidContext(this@NadoSunBaeApplication)
-            modules(viewModelModule)
-            modules(networkModule)
-            modules(dataSourceModule)
-            modules(repositoryModule)
-            modules(useCaseModule)
-            modules(apiModule)
         }
     }
 }
