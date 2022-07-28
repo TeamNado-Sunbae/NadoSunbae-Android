30ec889da8576e730705af0a39b4dd24b43f0510
ho-taek <kht072623@naver.com>
2022-07-28 19:20:35 +0900

    feat : 상단 탭 부분 커스텀뷰로 구현

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/community/CommunityFragment.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/community/CommunityFragment.kt
@@ -2,10 +2,10 @@ package com.nadosunbae_android.app.presentation.ui.community
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import com.nadosunbae_android.app.R
 import com.nadosunbae_android.app.databinding.FragmentCommunityBinding
 import com.nadosunbae_android.app.presentation.base.BaseFragment
+import com.nadosunbae_android.app.util.changeFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,5 +13,13 @@ class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragme
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initCommunityMainContent()
+    }
+
+
+
+    //커뮤니티 메인 콘텐트 붙이기
+    private fun initCommunityMainContent(){
+        changeFragment(R.id.fragment_container_community, CommunityMainContentFragment() )
     }
 }
