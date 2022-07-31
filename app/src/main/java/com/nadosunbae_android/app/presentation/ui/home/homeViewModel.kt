9df64124c56ca0713ee78207b9cac6e25cdffb1e
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-01 00:49:14 +0900

    [FEAT] #542 - add adapter

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/homeViewModel.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/homeViewModel.kt
@@ -1,6 +1,8 @@
 package com.nadosunbae_android.app.presentation.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.nadosunbae_android.domain.model.home.HomeCommunityData
+import com.nadosunbae_android.domain.model.home.HomeQuestionData
 import com.nadosunbae_android.domain.model.home.HomeReviewData
 
 class homeViewModel : ViewModel() {
@@ -12,5 +14,23 @@ class homeViewModel : ViewModel() {
         HomeReviewData("test","test","test"),
         HomeReviewData("test","test","test"),
         HomeReviewData("test","test","test")
+    )
+
+    val questionData = listOf<HomeQuestionData>(
+        HomeQuestionData("test", "test", "test"),
+        HomeQuestionData("test", "test", "test"),
+        HomeQuestionData("test", "test", "test"),
+        HomeQuestionData("test", "test", "test"),
+        HomeQuestionData("test", "test", "test"),
+        HomeQuestionData("test", "test", "test")
+    )
+
+    val communityData = listOf<HomeCommunityData>(
+        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
+        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
+        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
+        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
+        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
+        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1)
     )
 }
