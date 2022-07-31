f18447ccbdc72bd499266a49984317205ea03ba0
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-01 01:01:15 +0900

    [FEAT] #542 - setting max line and length

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/homeViewModel.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/homeViewModel.kt
@@ -6,31 +6,32 @@ import com.nadosunbae_android.domain.model.home.HomeQuestionData
 import com.nadosunbae_android.domain.model.home.HomeReviewData
 
 class homeViewModel : ViewModel() {
+    //더미데이터 테스트 -> 뷰 깨지는지 확인
     val reviewData = listOf<HomeReviewData>(
-        HomeReviewData("test","test","test"),
-        HomeReviewData("test","test","test"),
-        HomeReviewData("test","test","test"),
-        HomeReviewData("test","test","test"),
-        HomeReviewData("test","test","test"),
-        HomeReviewData("test","test","test"),
-        HomeReviewData("test","test","test")
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23"),
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23"),
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23"),
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23"),
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23"),
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23"),
+        HomeReviewData("경영학과","난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어최대 40자","21/12/23")
     )
 
     val questionData = listOf<HomeQuestionData>(
-        HomeQuestionData("test", "test", "test"),
-        HomeQuestionData("test", "test", "test"),
-        HomeQuestionData("test", "test", "test"),
-        HomeQuestionData("test", "test", "test"),
-        HomeQuestionData("test", "test", "test"),
-        HomeQuestionData("test", "test", "test")
+        HomeQuestionData("질문자닉네임", "209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...", "21/12/23"),
+        HomeQuestionData("질문자닉네임", "209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...", "21/12/23"),
+        HomeQuestionData("질문자닉네임", "209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...", "21/12/23"),
+        HomeQuestionData("질문자닉네임", "209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...", "21/12/23"),
+        HomeQuestionData("질문자닉네임", "209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...", "21/12/23"),
+        HomeQuestionData("질문자닉네임", "209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...209*66 넘으면 ...난 자유롭고 싶어 지금 전투력 수치 111퍼입고싶은 옷 입고싶어 싶은 옷입고싶...", "21/12/23")
     )
 
     val communityData = listOf<HomeCommunityData>(
-        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
-        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
-        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
-        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
-        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1),
-        HomeCommunityData("Test", "Test", "Test", "Test", "Test", 1,1)
+        HomeCommunityData("질문", "커뮤니티 제목", "커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용", "학과전체", "오후 5:42", 1,1),
+        HomeCommunityData("질문", "커뮤니티 제목", "커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용", "학과전체", "오후 5:42", 1,1),
+        HomeCommunityData("질문", "커뮤니티 제목", "커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용", "학과전체", "오후 5:42", 1,1),
+        HomeCommunityData("질문", "커뮤니티 제목", "커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용", "학과전체", "오후 5:42", 1,1),
+        HomeCommunityData("질문", "커뮤니티 제목", "커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용", "학과전체", "오후 5:42", 1,1),
+        HomeCommunityData("질문", "커뮤니티 제목", "커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용 커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용커뮤니티내용 커뮤니티내용", "학과전체", "오후 5:42", 1,1)
     )
 }
