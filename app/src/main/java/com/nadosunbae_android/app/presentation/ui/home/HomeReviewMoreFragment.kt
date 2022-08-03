082b9d4ee14374452101cf2780a0bcafc37b9f8e
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-03 23:48:05 +0900

    [FEAT] #549 - review view

---
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/HomeReviewMoreFragment.kt
@@ -0,0 +1,18 @@
+package com.nadosunbae_android.app.presentation.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.nadosunbae_android.app.R
+import com.nadosunbae_android.app.databinding.FragmentHomeReviewMoreBinding
+import com.nadosunbae_android.app.presentation.base.BaseFragment
+
+
+class HomeReviewMoreFragment : BaseFragment<FragmentHomeReviewMoreBinding>(R.layout.fragment_home_review_more) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
