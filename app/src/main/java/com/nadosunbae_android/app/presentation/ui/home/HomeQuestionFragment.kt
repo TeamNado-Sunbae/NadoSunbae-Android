41a00867f8a27cd94642e0fefc3cd6e7c548a9dc
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-08-04 22:24:37 +0900

    [FEAT] #549 - add question view

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/HomeQuestionFragment.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/HomeQuestionFragment.kt
@@ -1,18 +1,17 @@
 package com.nadosunbae_android.app.presentation.ui.home
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.nadosunbae_android.app.R
-import com.nadosunbae_android.app.databinding.FragmentHomeReviewMoreBinding
+import com.nadosunbae_android.app.databinding.FragmentHomeQuestionBinding
 import com.nadosunbae_android.app.presentation.base.BaseFragment
 
-
-class HomeReviewMoreFragment : BaseFragment<FragmentHomeReviewMoreBinding>(R.layout.fragment_home_review_more) {
-
+class HomeQuestionFragment : BaseFragment<FragmentHomeQuestionBinding>(R.layout.fragment_home_question) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
-
-
 }
---
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/HomeReviewFragment.kt
@@ -0,0 +1,18 @@
+package com.nadosunbae_android.app.presentation.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.nadosunbae_android.app.R
+import com.nadosunbae_android.app.databinding.FragmentHomeReviewBinding
+import com.nadosunbae_android.app.presentation.base.BaseFragment
+
+
+class HomeReviewFragment : BaseFragment<FragmentHomeReviewBinding>(R.layout.fragment_home_review) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
