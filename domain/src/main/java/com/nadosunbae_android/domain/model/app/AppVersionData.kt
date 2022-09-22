354315724297db9a46e19d76bf9f357a62607535
ho-taek <kht072623@naver.com>
2022-09-22 22:39:05 +0900

    feat : app 버전 구분

--- domain/src/main/java/com/nadosunbae_android/domain/model/app/AppVersionData.kt
+++ domain/src/main/java/com/nadosunbae_android/domain/model/app/AppVersionData.kt
@@ -2,4 +2,10 @@ package com.nadosunbae_android.domain.model.app
 
 data class AppVersionData(
     val AOS: String
-)
+){
+    companion object{
+        val DEFAULT = AppVersionData(
+            AOS = ""
+        )
+    }
+}
