82b12feb04cbea51c4829d4c6d114af796d33fae
ho-taek <71322949+ho-taek@users.noreply.github.com>
2022-09-23 15:23:10 +0900

    Feat : 앱 버전별 업데이트 관리 (#609)
    * feat : 앱 버전 서버 통신 연결
    
    * feat : app 버전 구분
    
    * feat : 앱 버전별 업데이트 관리 완료

--- data/src/main/java/com/nadosunbae_android/data/datasource/remote/app/AppDataSource.kt
+++ data/src/main/java/com/nadosunbae_android/data/datasource/remote/app/AppDataSource.kt
@@ -1,7 +1,11 @@
 package com.nadosunbae_android.data.datasource.remote.app
 
 import com.nadosunbae_android.data.model.response.app.ResponseAppBanner
+import com.nadosunbae_android.data.model.response.app.ResponseAppVersion
 
 interface AppDataSource {
     suspend fun getAppBanner(type : String) : ResponseAppBanner
+
+    //앱 버전
+    suspend fun getAppVersion() : ResponseAppVersion
 }
--- data/src/main/java/com/nadosunbae_android/data/datasource/remote/app/AppDataSourceImpl.kt
+++ data/src/main/java/com/nadosunbae_android/data/datasource/remote/app/AppDataSourceImpl.kt
@@ -2,10 +2,15 @@ package com.nadosunbae_android.data.datasource.remote.app
 
 import com.nadosunbae_android.data.api.app.AppService
 import com.nadosunbae_android.data.model.response.app.ResponseAppBanner
+import com.nadosunbae_android.data.model.response.app.ResponseAppVersion
 import javax.inject.Inject
 
 class AppDataSourceImpl @Inject constructor(private val service: AppService) : AppDataSource {
     override suspend fun getAppBanner(type: String): ResponseAppBanner {
         return service.getAppBanner(type)
+    }
+
+    override suspend fun getAppVersion(): ResponseAppVersion {
+        return service.getAppVersion()
     }
 }
