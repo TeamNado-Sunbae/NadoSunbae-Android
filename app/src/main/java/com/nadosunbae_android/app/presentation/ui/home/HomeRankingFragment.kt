fccb0b385f58b39d25d8ad3012796f9a441c63a7
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-10-05 21:18:27 +0900

    [FEAT] #639 - 이미지 통일

--- app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/HomeRankingFragment.kt
+++ app/src/main/java/com/nadosunbae_android/app/presentation/ui/home/HomeRankingFragment.kt
@@ -9,18 +9,20 @@ import androidx.navigation.fragment.findNavController
 import com.nadosunbae_android.app.R
 import com.nadosunbae_android.app.databinding.FragmentHomeRankingBinding
 import com.nadosunbae_android.app.presentation.base.BaseFragment
+import com.nadosunbae_android.app.presentation.ui.classroom.question.DataToFragment
 import com.nadosunbae_android.app.presentation.ui.home.adpter.RankingDetailAdapter
 import com.nadosunbae_android.app.presentation.ui.main.MainGlobals
 import com.nadosunbae_android.app.presentation.ui.main.viewmodel.MainViewModel
 import com.nadosunbae_android.domain.model.home.HomeRankingData
 import dagger.hilt.android.AndroidEntryPoint
 import okhttp3.internal.toImmutableList
+import timber.log.Timber
 
 
 @AndroidEntryPoint
 class HomeRankingFragment :
     BaseFragment<FragmentHomeRankingBinding>(R.layout.fragment_home_ranking) {
-
+    var link = SeniorDataToFragment()
     private val mainViewModel : MainViewModel by activityViewModels()
     private val homeViewModel : HomeViewModel by viewModels()
     private lateinit var rankingDetailAdapter: RankingDetailAdapter
@@ -51,12 +53,32 @@ class HomeRankingFragment :
     }
 
     private fun initSetting() {
-        rankingDetailAdapter = RankingDetailAdapter(MainGlobals.signInData?.userId ?: 0)
+        rankingDetailAdapter = RankingDetailAdapter(MainGlobals.signInData?.userId ?: 0, link)
         binding.rvHomeReview.adapter = rankingDetailAdapter
         homeViewModel.getHomeRanking(mainViewModel.univId.value ?: 0)
         homeViewModel.rankingData.observe(viewLifecycleOwner) {
             (binding.rvHomeReview.adapter as RankingDetailAdapter).submitList(it.subList(0,30))
 
+        }
+    }
+
+    inner class SeniorDataToFragment : DataToFragment {
+        override fun getSeniorId(seniorId: Int){
+            mainViewModel.seniorId.value = seniorId
+            goMyPage(seniorId)
+        }
+    }
+
+    //선배 Id = userId가 같을 경우 마이페이지로 이동
+    private fun goMyPage(seniorId : Int){
+        val userId = mainViewModel.userId.value ?: 0
+        Timber.d("userId : $userId")
+        Timber.d("seniorId : $seniorId")
+        if(userId == seniorId){
+            mainViewModel.bottomNavItem.value = 4
+        }else{
+            mainViewModel.homeFragmentNum.value = 1
+            mainViewModel.initLoading.value = true
         }
     }
 
